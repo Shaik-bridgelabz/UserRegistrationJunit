229dd84f55fb3a8c12a705dabdb433e63882aaf1
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-29 00:42:25 +0530

    Add. Adding testcases of [Usecase4] to the repository.

--- test/java/com/bridgelabz/UserRegistrationTest.java
+++ test/java/com/bridgelabz/UserRegistrationTest.java
@@ -8,39 +8,53 @@ public class UserRegistrationTest {
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
         boolean result = validator.validateFirstName("Shaik");
-        Assert.assertTrue(true);
+        Assert.assertEquals(true,result);
     }
     @Test
     public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
         UserValidator validator = new UserValidator();
         boolean result1= validator.validateFirstName("sh");
-        Assert.assertFalse(false);
+        Assert.assertEquals(true,result1);
     }
 
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
         boolean result = validator.validateLasttName("Mohammed");
-        Assert.assertTrue(true);
+        Assert.assertEquals(true,result);
     }
     @Test
     public void givenLastName_WhenNotProper_ShouldReturnFalse() {
         UserValidator validator = new UserValidator();
         boolean result1= validator.validateLasttName("Mo");
-        Assert.assertFalse(false);
+        Assert.assertEquals(true,result1);
     }
 
     @Test
     public void givenEmail_WhenValid_ShouldReturnTrue() {
         UserValidator validator = new UserValidator();
         boolean result=validator.validateEmail("abc@bridgelabz.com");
-        Assert.assertTrue(true);
+        Assert.assertEquals(true,result);
     }
 
     @Test
     public void givenEmail_WhenNotValid_ShouldReturnFalse() {
         UserValidator validator = new UserValidator();
         boolean result1=validator.validateEmail("abc.@bridgelabz.com");
-        Assert.assertFalse(false);
+        Assert.assertEquals(true,result1);
+    }
+
+    @Test
+    public void givenPassword_WhenValid_ShouldReturnTrue() {
+        UserValidator validator = new UserValidator();
+        boolean result=validator.validatePassword("Shaik@123");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenPassword_WhenNotValid_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result1=validator.validatePassword("shaik123");
+        Assert.assertEquals(true,result1);
     }
 }
