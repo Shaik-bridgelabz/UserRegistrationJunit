c0646a42060d3d4e9744e2d95d0df2660ec54d6a
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-29 00:57:50 +0530

    Refactor. Modifying the main class to run testcases of [Usecase].

--- main/java/com/bridgelabz/UserValidator.java
+++ main/java/com/bridgelabz/UserValidator.java
@@ -6,7 +6,9 @@ public class UserValidator {
 
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
     private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
-    private static final String EMAIL_ADDRESS_PATTERN ="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[0-9a-zA-Z]?.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
+    private static final String EMAIL_ADDRESS_PATTERN ="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}+([.][a-zA-Z]{2})?$";
+    private static final String MOBILE_NUMBER_PATTERN = "^[0-9]{2}[ ][0-9]{10}$";
+
     public boolean validateFirstName(String fName)
     {
         Pattern pattern=Pattern.compile(FIRST_NAME_PATTERN);
@@ -25,4 +27,9 @@ public class UserValidator {
         return pattern.matcher(email).matches();
     }
 
+    public boolean validateMobileNumber(String phone)
+    {
+        Pattern pattern=Pattern.compile(MOBILE_NUMBER_PATTERN);
+        return pattern.matcher(phone).matches();
+    }
 }
