5e18f28c03700a8cb20d16ae3431a15ab4c4304b
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-29 00:16:20 +0530

    Refactor. Modifying the main class to run testcases of [Usecase2].

--- main/java/com/bridgelabz/UserValidator.java
+++ main/java/com/bridgelabz/UserValidator.java
@@ -5,11 +5,16 @@ import java.util.regex.Pattern;
 public class UserValidator {
 
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
-
+    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
     public boolean validateFirstName(String fName)
     {
         Pattern pattern=Pattern.compile(FIRST_NAME_PATTERN);
         return pattern.matcher(fName).matches();
     }
 
+    public boolean validateLasttName(String lName)
+    {
+        Pattern pattern=Pattern.compile(LAST_NAME_PATTERN);
+        return pattern.matcher(lName).matches();
+    }
 }
