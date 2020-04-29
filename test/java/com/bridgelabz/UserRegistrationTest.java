8a1992a48ac8e00d5772c5f512e3a8d18d5b34e0
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-29 15:08:42 +0530

    Add. Adding testcases of [Usecase7] to the repository.

--- test/java/com/bridgelabz/UserRegistrationTest.java
+++ test/java/com/bridgelabz/UserRegistrationTest.java
@@ -6,69 +6,103 @@ import org.junit.Test;
 public class UserRegistrationTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
-        UserValidator validator = new UserValidator();
-        boolean result = validator.validateFirstName("Shaik");
-        Assert.assertEquals(true,result);
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateFirstName("Shaik");
+        Assert.assertTrue(result);
     }
     @Test
     public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
-        UserValidator validator = new UserValidator();
-        boolean result1= validator.validateFirstName("sh");
-        Assert.assertEquals(true,result1);
+        UserValidator userValidator = new UserValidator();
+        boolean result1= userValidator.validateFirstName("sh");
+        Assert.assertFalse(result1);
     }
 
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
-        UserValidator validator = new UserValidator();
-        boolean result = validator.validateLasttName("Mohammed");
-        Assert.assertEquals(true,result);
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLasttName("Mohammed");
+        Assert.assertTrue(result);
     }
     @Test
     public void givenLastName_WhenNotProper_ShouldReturnFalse() {
-        UserValidator validator = new UserValidator();
-        boolean result1= validator.validateLasttName("Mo");
-        Assert.assertEquals(true,result1);
+        UserValidator userValidator = new UserValidator();
+        boolean result1= userValidator.validateLasttName("Mo");
+        Assert.assertFalse(result1);
     }
 
     @Test
     public void givenEmail_WhenValid_ShouldReturnTrue() {
-        UserValidator validator = new UserValidator();
-        boolean result=validator.validateEmail("abc@bridgelabz.com");
-        Assert.assertEquals(true,result);
+        UserValidator userValidator = new UserValidator();
+        boolean result=userValidator.validateEmail("abc@bridgelabz.com");
+        Assert.assertTrue(result);
     }
 
     @Test
     public void givenEmail_WhenNotValid_ShouldReturnFalse() {
-        UserValidator validator = new UserValidator();
-        boolean result1=validator.validateEmail("abc.@bridgelabz.com");
-        Assert.assertEquals(true,result1);
+        UserValidator userValidator = new UserValidator();
+        boolean result1=userValidator.validateEmail("abc.@bridgelabz.com");
+        Assert.assertFalse(result1);
     }
 
     @Test
     public void givenMobileNumber_WhenValid_ShouldReturnTrue() {
-        UserValidator validator = new UserValidator();
-        boolean result=validator.validateMobileNumber("91 8660424568");
-        Assert.assertEquals(true,result);
+        UserValidator userValidator = new UserValidator();
+        boolean result=userValidator.validateMobileNumber("91 8660424568");
+        Assert.assertTrue(result);
     }
 
     @Test
     public void givenMobileNumber_WhenNotValid_ShouldReturnFalse() {
-        UserValidator validator = new UserValidator();
-        boolean result1=validator.validateMobileNumber("8660424568");
-        Assert.assertEquals(true,result1);
+        UserValidator userValidator = new UserValidator();
+        boolean result1=userValidator.validateMobileNumber("8660424568");
+        Assert.assertFalse(result1);
+    }
+    @Test
+    public void givenMobileNumber_WithCharacters_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result1=userValidator.validateMobileNumber("ABc0424568");
+        Assert.assertFalse(result1);
     }
 
     @Test
-    public void givenPassword_WhenValid_ShouldReturnTrue() {
-        UserValidator validator = new UserValidator();
-        boolean result=validator.validatePassword("Shaikmohammed");
-        Assert.assertEquals(true,result);
+    public void givenPassword_HasMoreThan8Characters_ShouldReturnTrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean result=userValidator.validatePassword1("shaikmohammed");
+        Assert.assertTrue(result);
     }
 
     @Test
-    public void givenPassword_WhenNotValid_ShouldReturnFalse() {
-        UserValidator validator = new UserValidator();
-        boolean result1=validator.validatePassword("shaikmoha");
-        Assert.assertEquals(true,result1);
+    public void givenPassword_HasLessThan8Charcters_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result1=userValidator.validatePassword1("shaik");
+        Assert.assertFalse(result1);
+    }
+
+    @Test
+    public void givenPassword_HasAtleast_1UpperCase_ShouldReturnTrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean result=userValidator.validatePassword2("Shaikmohammed");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPassword_HasNo_Uppercase_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result1=userValidator.validatePassword2("shaik");
+        Assert.assertFalse(result1);
+    }
+
+    @Test
+    public void givenPassword_HasAtleast_1NumericNumber_ShouldReturnTrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean result=userValidator.validatePassword3("Shaikmohammed12");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenPassword_HasNo_NumericNumber_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result1=userValidator.validatePassword3("shaikmohammed");
+        Assert.assertFalse(result1);
     }
 }
